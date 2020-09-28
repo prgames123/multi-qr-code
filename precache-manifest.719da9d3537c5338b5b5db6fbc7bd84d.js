0c0c92a1069618a31d142fbdf0abce7f89def45c
Pedro Ryan <53787493+prgames123@users.noreply.github.com>
2020-09-28 19:20:23 -0300

    Updates

---
+++ precache-manifest.719da9d3537c5338b5b5db6fbc7bd84d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96b0523d39314ec12257433db344bcd2",
+    "url": "/multi-qr-code/index.html"
+  },
+  {
+    "revision": "beb92499090bb2ebca72",
+    "url": "/multi-qr-code/static/css/main.6465a6b7.chunk.css"
+  },
+  {
+    "revision": "2987326ddeaff9fa9182",
+    "url": "/multi-qr-code/static/js/2.692afb4d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/multi-qr-code/static/js/2.692afb4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beb92499090bb2ebca72",
+    "url": "/multi-qr-code/static/js/main.54f4863b.chunk.js"
+  },
+  {
+    "revision": "d9b9a3ce852953f3be04",
+    "url": "/multi-qr-code/static/js/runtime-main.a157ebc1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/multi-qr-code/static/media/logo.5d5d9eef.svg"
+  }
+]);
